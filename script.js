f9e55035c25a6d08722ca0873924e19b6b5c2260
synchronizer <synchronizer@yandex.ru>
2025-09-17 14:59:13 +0300

    commit

--- script.js
+++ script.js
@@ -36,6 +36,24 @@ Array.from(document.querySelectorAll('.notifications__item')).forEach(notificati
 })
 
 
+Array.from(document.querySelectorAll('.about-preview')).forEach(block => {
+    block.classList.add('about-preview_hide')
+
+    const observer = new IntersectionObserver(
+        entries => {
+            entries.forEach(entry => {
+                console.log(entry)
+                entry.target.classList.toggle('about-preview_hide', !entry.isIntersecting)
+                if (entry.isIntersecting) observer.unobserve(entry.target);
+            })
+        }, {
+        threshold: .3
+    }
+    )
+
+    observer.observe(block)
+})
+
 Array.from(document.querySelectorAll('.call-request')).forEach(item => {
     const title = item.querySelector('.call-request__title'),
         name = item.querySelector('[name="name"]'),
